0e90e527e6dcc0c5f392322a24dc8ea11b5023e3
Curtis Sera <csera@ucsd.edu>
2020-11-06 13:58:15 -0800

    Compiled plots Base v2.1.1, Cap and Body v2.0.4
    Gathered all desired output from BaseFusionPrePost v2.1.1, BodyFusionPrePost v2.0.4, CapFusionPrePost v2.0.4 --> "Output figure\Before vs after fusion 2020-11-06"
    *Note that BaseFusionPrePost and its output show changes, but that's only because I re-ran the notebook's cells
    
    FusionPrePostPlot.py v2.0:
    - Graph new data against each other and apply various transformations
    - Fixed figure annotation issue of LaTeX no showing up at all
       - Unfortunately, there doesn't seem to be any reliable fix from Plotly, and properly showing fractions requires LaTex since you can't do that in HTML alone.   I therefore unfortunately had to switch everything from Plotly.py --> Matplotlib.pyplot

--- Python/FusionPrePostPlot.py
+++ Python/FusionPrePostPlot.py
@@ -1,11 +1,12 @@
 # Curtis Sera
-# v1.0 for 
-#    v2.0 of CapFusionPrePost and BodyFusionPrePost
-#    v2.1 of BaseFusionPrePost
+# v2.0 for 
+#    v2.0.x of CapFusionPrePost and BodyFusionPrePost
+#    v2.1.x of BaseFusionPrePost
+# Using *MATPLOTLIB*, not Plotly since req LaTeX
 # 2020-10-21
 
 import numpy as np
-import plotly.graph_objs as go
+import matplotlib.pyplot as pt
 
 capDGb = np.genfromtxt("output data/Cap DGb 2-0.csv",delimiter=',')
 cap_rt = np.genfromtxt("output data/Cap rt 2-0.csv", delimiter=',')
@@ -22,43 +23,68 @@ print('bodyDGb = ',bodyDGb)
 
 print('Plotting energies')
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=cap_rt, y=capDGb, mode='markers + lines', name='Cap'))
-fig.add_trace(go.Scatter(x=body_rt, y=bodyDGb, mode='markers + lines', name='Body'))
-fig.add_trace(go.Scatter(x=base_rt, y=baseDGb, mode='markers + lines', name='Base'))
-fig.update_layout(title="$Compiled: \\quad \Delta G_b = G_b^{post} - G_b^{pre}$",
-                        xaxis_title="$r_{t,rim} \\, (nm)$", yaxis_title="Energy (kT)")
-fig.show()
+print('making linear plot')
+linFig = pt.figure()
+linSP = linFig.add_subplot(111)
+linSP.plot(cap_rt, capDGb, label='Cap')
+linSP.plot(body_rt, bodyDGb, label='Body')
+linSP.plot(base_rt, baseDGb, label='Base')
+linSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+linSP.set_ylabel('$Energy \\, (kT)$')
+linSP.set_title('$Compiled: \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
+linSP.legend()
 
-logFig = go.Figure()
-logFig.add_trace(go.Scatter(x=cap_rt, y=capDGb, mode='markers + lines', name='Cap'))
-logFig.add_trace(go.Scatter(x=body_rt, y=bodyDGb, mode='markers + lines', name='Body'))
-logFig.add_trace(go.Scatter(x=base_rt, y=baseDGb, mode='markers + lines', name='Base'))
-logFig.update_layout(title="$Compiled: \\quad \Delta G_b = G_b^{post} - G_b^{pre}$",
-                        xaxis_title="$r_{t,rim} \\, (nm)$", yaxis_title="Energy (kT)")
-logFig.update_yaxes(type='log')
-logFig.show()
+print('making log-linear (semilog) plot')
+logFig = pt.figure()
+logSP = logFig.add_subplot(111)
+logSP.plot(cap_rt, capDGb, label='Cap')
+logSP.plot(body_rt, bodyDGb, label='Body')
+logSP.plot(base_rt, baseDGb, label='Base')
+logSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+logSP.set_ylabel('$Energy \\, (kT)$')
+logSP.set_yscale('log')
+logSP.set_title('$Compiled \\, (semilog\, scale): \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
+logSP.legend()
 
-ratlFig = go.Figure()
-ratlFig.add_trace(go.Scatter(x=cap_rt, y=1/capDGb, mode='markers + lines', name='Cap'))
-ratlFig.add_trace(go.Scatter(x=body_rt, y=1/bodyDGb, mode='markers + lines', name='Body'))
-ratlFig.add_trace(go.Scatter(x=base_rt, y=1/baseDGb, mode='markers + lines', name='Base'))
-ratlFig.update_layout(title="$Compiled: \\quad \\frac{1}{\Delta G_b} = \\frac{1}{G_b^{post} - G_b^{pre}}$",
-                        xaxis_title="$r_{t,rim} \\, (nm)$", yaxis_title="$Energy^{-1} \\, (kT^{-1})$")
-ratlFig.show()
+print("making reciprocal (ie rational) plot")
+ratlFig = pt.figure()
+ratlSP = ratlFig.add_subplot(111)
+ratlSP.plot(cap_rt, 1/capDGb, label='Cap')
+ratlSP.plot(body_rt, 1/bodyDGb, label='Body')
+ratlSP.plot(base_rt, 1/baseDGb, label='Base')
+ratlSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSP.set_ylabel('$Energy^{-1} \\, (kT^{-1})$')
+ratlSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b} = \\frac{1}{G_b^{post} - G_b^{pre}}$')
+ratlSP.legend()
 
-ratlFigSqrt2 = go.Figure()
-ratlFigSqrt2.add_trace(go.Scatter(x=cap_rt, y=1/(capDGb**2), mode='markers + lines', name='Cap'))
-ratlFigSqrt2.add_trace(go.Scatter(x=body_rt, y=1/(bodyDGb**2), mode='markers + lines', name='Body'))
-ratlFigSqrt2.add_trace(go.Scatter(x=base_rt, y=1/(baseDGb**2), mode='markers + lines', name='Base'))
-ratlFigSqrt2.update_layout(title="$Compiled: \\quad \\frac{1}{(\Delta G_b)^{2}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{2}}$",
-                        xaxis_title="$r_{t,rim} \\, (nm)$", yaxis_title="$Energy^{-2} \\, (kT^{-2})$")
-ratlFigSqrt2.show()
+print('making reciprocal sqr')
+ratlSqrFig = pt.figure()
+ratlSqrSP = ratlSqrFig.add_subplot(111)
+ratlSqrSP.plot(cap_rt, 1/(capDGb**2), label='Cap')
+ratlSqrSP.plot(body_rt, 1/(bodyDGb**2), label='Body')
+ratlSqrSP.plot(base_rt, 1/(baseDGb**2), label='Base')
+ratlSqrSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSqrSP.set_ylabel('$Energy^{-2} \\, (kT^{-2})$')
+ratlSqrSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^2} = \\frac{1}{(G_b^{post} - G_b^{pre})^2}$')
+ratlSqrSP.legend()
 
-ratlFigSqrt = go.Figure()
-ratlFigSqrt.add_trace(go.Scatter(x=cap_rt, y=1/(capDGb**0.5), mode='markers + lines', name='Cap'))
-ratlFigSqrt.add_trace(go.Scatter(x=body_rt, y=1/(bodyDGb**0.5), mode='markers + lines', name='Body'))
-ratlFigSqrt.add_trace(go.Scatter(x=base_rt, y=1/(baseDGb**0.5), mode='markers + lines', name='Base'))
-ratlFigSqrt.update_layout(title="$Compiled: \\quad \\frac{1}{(\Delta G_b)^{0.5}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{0.5}}$",
-                        xaxis_title="$r_{t,rim} \\, (nm)$", yaxis_title="$Energy^{-0.5} \\, (kT^{-0.5})$")
-ratlFigSqrt.show()
+print('making reciprocal sqrt')
+ratlSqrtFig = pt.figure()
+ratlSqrtSP = ratlSqrtFig.add_subplot(111)
+ratlSqrtSP.plot(cap_rt, 1/(capDGb**0.5), label='Cap')
+ratlSqrtSP.plot(body_rt, 1/(bodyDGb**0.5), label='Body')
+ratlSqrtSP.plot(base_rt, 1/(baseDGb**0.5), label='Base')
+ratlSqrtSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSqrtSP.set_ylabel('$Energy^{-1/2} \\, (kT^{-1/2})$')
+ratlSqrtSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^{1/2}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{1/2}}$')
+ratlSqrtSP.legend()
+
+pt.show()
+
+# Save plots
+print('saving plots as PDFs')
+linFig.savefig("output data/Compiled DGb linear.pdf")
+logFig.savefig("output data/Compiled DGb semilog.pdf")
+ratlFig.savefig("output data/Compiled DGb rational.pdf")
+ratlSqrFig.savefig("output data/Compiled DGb ratl-sqr.pdf")
+ratlSqrtFig.savefig("output data/Compiled DGb ratlsqrt.pdf")
