1ef6661ad023b56b8119cf6304cf9370d3e6474c
Curtis Sera <csera@ucsd.edu>
2020-11-09 17:42:04 -0800

    FusionPrePost v2.2
    Updated base, body, and cap scripts to use np.linspace(2,55,num=150) for rim_rt rather than a manually defined array
    
    Edited x axis labels for graphs to make them more descriptive: r_{t,rim} --> r_{t,protr} for protrusion
    
    Figures and data copied to "Output figures" folder

--- Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
+++ Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
@@ -1,21 +1,20 @@
 # Curtis Sera
-# v2.0 for 
-#    v2.0.x of CapFusionPrePost and BodyFusionPrePost
-#    v2.1.x of BaseFusionPrePost
+# v2.2 for 
+#    v2.2.x of CapFusionPrePost, BodyFusionPrePost, and BaseFusionPrePost
 # Using *MATPLOTLIB*, not Plotly since req LaTeX
 # 2020-10-21
 
 import numpy as np
 import matplotlib.pyplot as pt
 
-capDGb = np.genfromtxt("output data/Cap DGb 2-0.csv",delimiter=',')
-cap_rt = np.genfromtxt("output data/Cap rt 2-0.csv", delimiter=',')
+capDGb = np.genfromtxt("output data/Cap DGb 2-2.csv",delimiter=',')
+cap_rt = np.genfromtxt("output data/Cap rt 2-2.csv", delimiter=',')
 
-bodyDGb = np.genfromtxt("output data/Body DGb 2-0.csv",delimiter=',')
-body_rt = np.genfromtxt("output data/Body rt 2-0.csv", delimiter=',')
+bodyDGb = np.genfromtxt("output data/Body DGb 2-2.csv",delimiter=',')
+body_rt = np.genfromtxt("output data/Body rt 2-2.csv", delimiter=',')
 
-baseDGb = np.genfromtxt("output data/Base min path 2-1.csv",delimiter=',')
-base_rt = np.genfromtxt("output data/Base rt 2-1.csv", delimiter=',')
+baseDGb = np.genfromtxt("output data/Base min path 2-2.csv",delimiter=',')
+base_rt = np.genfromtxt("output data/Base rt 2-2.csv", delimiter=',')
 
 print('base_rt = ',base_rt)
 print('baseDGb = ',baseDGb)
@@ -29,7 +28,7 @@ linSP = linFig.add_subplot(111)
 linSP.plot(cap_rt, capDGb, label='Cap')
 linSP.plot(body_rt, bodyDGb, label='Body')
 linSP.plot(base_rt, baseDGb, label='Base')
-linSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+linSP.set_xlabel('$r_{t,protr} \\, (nm)$')
 linSP.set_ylabel('$Energy \\, (kT)$')
 linSP.set_title('$Compiled: \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
 linSP.legend()
@@ -40,7 +39,7 @@ logSP = logFig.add_subplot(111)
 logSP.plot(cap_rt, capDGb, label='Cap')
 logSP.plot(body_rt, bodyDGb, label='Body')
 logSP.plot(base_rt, baseDGb, label='Base')
-logSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+logSP.set_xlabel('$r_{t,protr} \\, (nm)$')
 logSP.set_ylabel('$Energy \\, (kT)$')
 logSP.set_yscale('log')
 logSP.set_title('$Compiled \\, (semilog\, scale): \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
@@ -52,7 +51,7 @@ ratlSP = ratlFig.add_subplot(111)
 ratlSP.plot(cap_rt, 1/capDGb, label='Cap')
 ratlSP.plot(body_rt, 1/bodyDGb, label='Body')
 ratlSP.plot(base_rt, 1/baseDGb, label='Base')
-ratlSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSP.set_xlabel('$r_{t,protr} \\, (nm)$')
 ratlSP.set_ylabel('$Energy^{-1} \\, (kT^{-1})$')
 ratlSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b} = \\frac{1}{G_b^{post} - G_b^{pre}}$')
 ratlSP.legend()
@@ -63,7 +62,7 @@ ratlSqrSP = ratlSqrFig.add_subplot(111)
 ratlSqrSP.plot(cap_rt, 1/(capDGb**2), label='Cap')
 ratlSqrSP.plot(body_rt, 1/(bodyDGb**2), label='Body')
 ratlSqrSP.plot(base_rt, 1/(baseDGb**2), label='Base')
-ratlSqrSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSqrSP.set_xlabel('$r_{t,protr} \\, (nm)$')
 ratlSqrSP.set_ylabel('$Energy^{-2} \\, (kT^{-2})$')
 ratlSqrSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^2} = \\frac{1}{(G_b^{post} - G_b^{pre})^2}$')
 ratlSqrSP.legend()
@@ -74,7 +73,7 @@ ratlSqrtSP = ratlSqrtFig.add_subplot(111)
 ratlSqrtSP.plot(cap_rt, 1/(capDGb**0.5), label='Cap')
 ratlSqrtSP.plot(body_rt, 1/(bodyDGb**0.5), label='Body')
 ratlSqrtSP.plot(base_rt, 1/(baseDGb**0.5), label='Base')
-ratlSqrtSP.set_xlabel('$r_{t,rim} \\, (nm)$')
+ratlSqrtSP.set_xlabel('$r_{t,protr} \\, (nm)$')
 ratlSqrtSP.set_ylabel('$Energy^{-1/2} \\, (kT^{-1/2})$')
 ratlSqrtSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^{1/2}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{1/2}}$')
 ratlSqrtSP.legend()
@@ -83,8 +82,8 @@ pt.show()
 
 # Save plots
 print('saving plots as PDFs')
-linFig.savefig("output data/Compiled DGb linear.pdf")
-logFig.savefig("output data/Compiled DGb semilog.pdf")
-ratlFig.savefig("output data/Compiled DGb rational.pdf")
-ratlSqrFig.savefig("output data/Compiled DGb ratl-sqr.pdf")
-ratlSqrtFig.savefig("output data/Compiled DGb ratlsqrt.pdf")
+linFig.savefig("output data/Compiled DGb linear 2-2.pdf")
+logFig.savefig("output data/Compiled DGb semilog 2-2.pdf")
+ratlFig.savefig("output data/Compiled DGb rational 2-2.pdf")
+ratlSqrFig.savefig("output data/Compiled DGb ratl-sqr 2-2.pdf")
+ratlSqrtFig.savefig("output data/Compiled DGb ratlsqrt 2-2.pdf")
