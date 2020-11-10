1aa76b22924323ae01dac58c884adb1937d52310
Curtis Sera <csera@ucsd.edu>
2020-11-09 17:56:06 -0800

    FusionPrePost v2.2.1 - edit axis labels
    Edited x axis labels to improve readability in figures: $r_{t,protr}$ --> Protrusion $r_{t}$
    
    Updated the v2.2 folder in "\Output figures\"

--- Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
+++ Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
@@ -1,8 +1,8 @@
 # Curtis Sera
-# v2.2 for 
+# v2.2.1 for 
 #    v2.2.x of CapFusionPrePost, BodyFusionPrePost, and BaseFusionPrePost
 # Using *MATPLOTLIB*, not Plotly since req LaTeX
-# 2020-10-21
+# 2020-11-09
 
 import numpy as np
 import matplotlib.pyplot as pt
@@ -28,7 +28,7 @@ linSP = linFig.add_subplot(111)
 linSP.plot(cap_rt, capDGb, label='Cap')
 linSP.plot(body_rt, bodyDGb, label='Body')
 linSP.plot(base_rt, baseDGb, label='Base')
-linSP.set_xlabel('$r_{t,protr} \\, (nm)$')
+linSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 linSP.set_ylabel('$Energy \\, (kT)$')
 linSP.set_title('$Compiled: \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
 linSP.legend()
@@ -39,7 +39,7 @@ logSP = logFig.add_subplot(111)
 logSP.plot(cap_rt, capDGb, label='Cap')
 logSP.plot(body_rt, bodyDGb, label='Body')
 logSP.plot(base_rt, baseDGb, label='Base')
-logSP.set_xlabel('$r_{t,protr} \\, (nm)$')
+logSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 logSP.set_ylabel('$Energy \\, (kT)$')
 logSP.set_yscale('log')
 logSP.set_title('$Compiled \\, (semilog\, scale): \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
@@ -51,7 +51,7 @@ ratlSP = ratlFig.add_subplot(111)
 ratlSP.plot(cap_rt, 1/capDGb, label='Cap')
 ratlSP.plot(body_rt, 1/bodyDGb, label='Body')
 ratlSP.plot(base_rt, 1/baseDGb, label='Base')
-ratlSP.set_xlabel('$r_{t,protr} \\, (nm)$')
+ratlSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSP.set_ylabel('$Energy^{-1} \\, (kT^{-1})$')
 ratlSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b} = \\frac{1}{G_b^{post} - G_b^{pre}}$')
 ratlSP.legend()
@@ -62,7 +62,7 @@ ratlSqrSP = ratlSqrFig.add_subplot(111)
 ratlSqrSP.plot(cap_rt, 1/(capDGb**2), label='Cap')
 ratlSqrSP.plot(body_rt, 1/(bodyDGb**2), label='Body')
 ratlSqrSP.plot(base_rt, 1/(baseDGb**2), label='Base')
-ratlSqrSP.set_xlabel('$r_{t,protr} \\, (nm)$')
+ratlSqrSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSqrSP.set_ylabel('$Energy^{-2} \\, (kT^{-2})$')
 ratlSqrSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^2} = \\frac{1}{(G_b^{post} - G_b^{pre})^2}$')
 ratlSqrSP.legend()
@@ -73,7 +73,7 @@ ratlSqrtSP = ratlSqrtFig.add_subplot(111)
 ratlSqrtSP.plot(cap_rt, 1/(capDGb**0.5), label='Cap')
 ratlSqrtSP.plot(body_rt, 1/(bodyDGb**0.5), label='Body')
 ratlSqrtSP.plot(base_rt, 1/(baseDGb**0.5), label='Base')
-ratlSqrtSP.set_xlabel('$r_{t,protr} \\, (nm)$')
+ratlSqrtSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSqrtSP.set_ylabel('$Energy^{-1/2} \\, (kT^{-1/2})$')
 ratlSqrtSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^{1/2}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{1/2}}$')
 ratlSqrtSP.legend()
