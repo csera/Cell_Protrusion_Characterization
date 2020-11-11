221622ebb40ce68a88e2c9e1ce375dd5c0062cd3
Curtis Sera <csera@ucsd.edu>
2020-11-10 17:28:26 -0800

    FusionPrePost v2.3.1 - added flat pore
    Adapted the method used in BaseFusionPrePost for calclating the pore energy in the bulk to calculate the pore energy in a flat membrane.  FlatFusionPrePost also compares the solution to Kozlov's 1989 solution.  (My model produces a significantly smaller bending energy function .)  This is then added to the "Compiled DGb..." plots

--- Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
+++ Python/.ipynb_checkpoints/FusionPrePostPlot-checkpoint.py
@@ -1,6 +1,7 @@
 # Curtis Sera
-# v2.3 for 
-#    v2.3.x of CapFusionPrePost, BodyFusionPrePost, and BaseFusionPrePost
+# v2.3.1 for 
+#    v2.3.x of CapFusionPrePost, BodyFusionPrePost, BaseFusionPrePost
+#    and FlatFusionPrePost
 # Using *MATPLOTLIB*, not Plotly since req LaTeX
 # 2020-11-09
 
@@ -16,6 +17,9 @@ body_rt = np.genfromtxt("output data/Body rt 2-3.csv", delimiter=',')
 baseDGb = np.genfromtxt("output data/Base min path 2-3.csv",delimiter=',')
 base_rt = np.genfromtxt("output data/Base rt 2-3.csv", delimiter=',')
 
+flatDGb = np.genfromtxt("output data/Flat DGb 2-3.csv",delimiter=',')
+flat_rt = np.genfromtxt("output data/flat rt 2-3.csv", delimiter=',')
+
 print('base_rt = ',base_rt)
 print('baseDGb = ',baseDGb)
 print('bodyDGb = ',bodyDGb)
@@ -28,6 +32,7 @@ linSP = linFig.add_subplot(111)
 linSP.plot(cap_rt, capDGb, label='Cap')
 linSP.plot(body_rt, bodyDGb, label='Body')
 linSP.plot(base_rt, baseDGb, label='Base')
+linSP.plot(flat_rt, flatDGb, label='Flat')
 linSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 linSP.set_ylabel('$Energy \\, (kT)$')
 linSP.set_title('$Compiled: \\quad \\Delta G_b = G_b^{post} - G_b^{pre}$')
@@ -39,6 +44,7 @@ logSP = logFig.add_subplot(111)
 logSP.plot(cap_rt, capDGb, label='Cap')
 logSP.plot(body_rt, bodyDGb, label='Body')
 logSP.plot(base_rt, baseDGb, label='Base')
+logSP.plot(flat_rt, flatDGb, label='Flat')
 logSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 logSP.set_ylabel('$Energy \\, (kT)$')
 logSP.set_yscale('log')
@@ -51,6 +57,7 @@ ratlSP = ratlFig.add_subplot(111)
 ratlSP.plot(cap_rt, 1/capDGb, label='Cap')
 ratlSP.plot(body_rt, 1/bodyDGb, label='Body')
 ratlSP.plot(base_rt, 1/baseDGb, label='Base')
+ratlSP.plot(flat_rt, 1/flatDGb, label='Flat')
 ratlSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSP.set_ylabel('$Energy^{-1} \\, (kT^{-1})$')
 ratlSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b} = \\frac{1}{G_b^{post} - G_b^{pre}}$')
@@ -62,6 +69,7 @@ ratlSqrSP = ratlSqrFig.add_subplot(111)
 ratlSqrSP.plot(cap_rt, 1/(capDGb**2), label='Cap')
 ratlSqrSP.plot(body_rt, 1/(bodyDGb**2), label='Body')
 ratlSqrSP.plot(base_rt, 1/(baseDGb**2), label='Base')
+ratlSqrSP.plot(flat_rt, 1/(flatDGb**2), label='Flat')
 ratlSqrSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSqrSP.set_ylabel('$Energy^{-2} \\, (kT^{-2})$')
 ratlSqrSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^2} = \\frac{1}{(G_b^{post} - G_b^{pre})^2}$')
@@ -73,6 +81,7 @@ ratlSqrtSP = ratlSqrtFig.add_subplot(111)
 ratlSqrtSP.plot(cap_rt, 1/(capDGb**0.5), label='Cap')
 ratlSqrtSP.plot(body_rt, 1/(bodyDGb**0.5), label='Body')
 ratlSqrtSP.plot(base_rt, 1/(baseDGb**0.5), label='Base')
+ratlSqrtSP.plot(flat_rt, 1/(flatDGb**0.5), label='Flat')
 ratlSqrtSP.set_xlabel('$Protrusion \\, r_{t} \\, (nm)$')
 ratlSqrtSP.set_ylabel('$Energy^{-1/2} \\, (kT^{-1/2})$')
 ratlSqrtSP.set_title('$Compiled: \\quad \\frac{1}{\Delta G_b^{1/2}} = \\frac{1}{(G_b^{post} - G_b^{pre})^{1/2}}$')
@@ -82,8 +91,8 @@ pt.show()
 
 # Save plots
 print('saving plots as PDFs')
-linFig.savefig("output data/Compiled DGb linear 2-2.pdf")
-logFig.savefig("output data/Compiled DGb semilog 2-2.pdf")
-ratlFig.savefig("output data/Compiled DGb rational 2-2.pdf")
-ratlSqrFig.savefig("output data/Compiled DGb ratl-sqr 2-2.pdf")
-ratlSqrtFig.savefig("output data/Compiled DGb ratlsqrt 2-2.pdf")
+linFig.savefig("output data/Compiled DGb linear 2-3.pdf")
+logFig.savefig("output data/Compiled DGb semilog 2-3.pdf")
+ratlFig.savefig("output data/Compiled DGb rational 2-3.pdf")
+ratlSqrFig.savefig("output data/Compiled DGb ratl-sqr 2-3.pdf")
+ratlSqrtFig.savefig("output data/Compiled DGb ratlsqrt 2-3.pdf")
